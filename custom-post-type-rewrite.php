6cd53d1d174dc0be6b6d5444813ed3dc0a855452
thingsym <thingsym@gmail.com>
2015-08-21 01:02:27 +0900

    refactoring

--- custom-post-type-rewrite.php
+++ custom-post-type-rewrite.php
@@ -1,20 +1,38 @@
 <?php
-/*
-Plugin Name: Custom Post Type Rewrite
-Plugin URI: https://github.com/thingsym/custom-post-type-rewrite
-Description: This WordPress plugin adds default custom post type permalinks.
-Version: 1.0.0
-Author: thingsym
-Author URI: http://www.thingslabo.com/
-License: GPL2
-Text Domain: custom-post-type-rewrite
-Domain Path: /languages/
-*/
+/**
+ * Plugin Name: Custom Post Type Rewrite
+ * Plugin URI: https://github.com/thingsym/custom-post-type-rewrite
+ * Description: This WordPress plugin adds default custom post type permalinks.
+ * Version: 1.0.0
+ * Author: thingsym
+ * Author URI: http://www.thingslabo.com/
+ * License: GPL2
+ * Text Domain: custom-post-type-rewrite
+ * Domain Path: /languages
+ */
+
+/**
+ *     Copyright 2015 thingsym (http://www.thingslabo.com/)
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ */
 
 class Custom_Post_Type_Rewrite {
 
 	public function __construct() {
-		add_action( 'wp_loaded', array( &$this, 'set_rewrite' ), 10 );
+		add_action( 'wp_loaded', array( $this, 'set_rewrite' ), 10 );
 	}
 
 	public function set_rewrite() {
@@ -40,8 +58,8 @@ class Custom_Post_Type_Rewrite {
 				continue;
 			}
 
-			$slug = get_post_type_object( $post_type )->rewrite['slug'] ? get_post_type_object( $post_type )->rewrite['slug'] : $post_type ;
-			$front = get_post_type_object( $post_type )->rewrite['with_front'] ? $wp_rewrite->front : '' ;
+			$slug = get_post_type_object( $post_type )->rewrite['slug'] ? get_post_type_object( $post_type )->rewrite['slug'] : $post_type;
+			$front = get_post_type_object( $post_type )->rewrite['with_front'] ? $wp_rewrite->front : '';
 
 			$replace = array( preg_replace( '/^\//', '', $front ), $slug, $wp_rewrite->rewritereplace[0], $wp_rewrite->rewritereplace[1], $wp_rewrite->rewritereplace[2], $date );
 
